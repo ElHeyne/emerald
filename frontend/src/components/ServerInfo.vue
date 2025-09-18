c410a8bea40d059da6132eee096195a82c641943
HY_ElCositas <erikvvh06@gmail.com>
2025-09-18 11:57:37 +0200

    Action bar visible
    - Added an action bar to the ServerInfo component.
    
    - Future implementation involves using the buttons to interact with the container.

--- frontend/src/components/ServerInfo.vue
+++ frontend/src/components/ServerInfo.vue
@@ -22,26 +22,69 @@ onMounted(async () => {
     state.isLoading = false
   }
 })
+
+const isActiveBullet = (buttonState) => {
+  if (state.isLoading) return false
+  const containerStatus = state.container.status
+  return containerStatus === buttonState
+}
 </script>
 
 <template>
   <section v-if="state.isLoading"><PulseLoader /></section>
-  <section v-if="!state.isLoading" class="flex">
-    <div>
-      <h1 class="text-2xl font-bold">{{ state.container.name }}</h1>
-      <i class="text-xs">{{ state.container.short_id }}</i>
-    </div>
-    <div>
-      <section
-        class="rounded-full px-4 py-0.5 ml-5"
-        :class="{
-          'bg-state-exited': state.container.status === 'exited',
-          'bg-state-running': state.container.status === 'running',
-          'bg-state-paused': state.container.status === 'paused',
-        }"
-      >
-        {{ state.container.status }}
-      </section>
-    </div>
-  </section>
+  <div v-if="!state.isLoading">
+    <section class="flex">
+      <div>
+        <h1 class="text-2xl font-bold">{{ state.container.name }}</h1>
+        <i class="text-xs">{{ state.container.short_id }}</i>
+      </div>
+      <div>
+        <section
+          class="rounded-full px-4 py-0.5 ml-5"
+          :class="{
+            'bg-state-exited': state.container.status === 'exited',
+            'bg-state-running': state.container.status === 'running',
+            'bg-state-paused': state.container.status === 'paused',
+          }"
+        >
+          {{ state.container.status }}
+        </section>
+      </div>
+    </section>
+
+    <section>
+      <div class="h-10 max-w-lg bg-em-gray-darker border bd-em-gray-darker rounded-lg">
+        <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
+          <button
+            class="inline-flex flex-col items-center justify-center px-5 rounded-s-lg hover:bg-green-500"
+            :class="[isActiveBullet('running') ? 'text-green-500 hover:text-white' : 'text-white']"
+          >
+            <a class="pi pi-play"></a>
+          </button>
+          <button
+            class="inline-flex flex-col items-center justify-center px-5 hover:bg-em-gray-light"
+            :class="[isActiveBullet('exited') ? 'text-red-500' : 'text-white']"
+          >
+            <a class="pi pi-stop"></a>
+          </button>
+          <button
+            class="inline-flex flex-col items-center justify-center px-5 hover:bg-em-gray-light"
+            :class="[isActiveBullet('paused') ? 'text-yellow-500' : 'text-white']"
+          >
+            <a class="pi pi-refresh"></a>
+          </button>
+          <button
+            class="inline-flex flex-col items-center justify-center px-5 hover:bg-em-gray-light"
+          >
+            <a class="pi pi-pencil"></a>
+          </button>
+          <button
+            class="inline-flex flex-col items-center justify-center px-5 rounded-e-lg bg-em-red hover:bg-em-red-dark"
+          >
+            <a class="pi pi-trash"></a>
+          </button>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
