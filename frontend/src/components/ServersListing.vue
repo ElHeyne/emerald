ac58ae5593d2d40fbb7a9496d0883b804d9835e9
HY_ElCositas <erikvvh06@gmail.com>
2025-09-18 10:16:32 +0200

    Fix: ServersListing No Servers message:
    - Now the 'Not Found' message works as intended when the url exists and the container object is empty
    
    - Added the error message for when the url is down or the url is incorrect and returns {"details": "Not Found"}

--- frontend/src/components/ServersListing.vue
+++ frontend/src/components/ServersListing.vue
@@ -8,19 +8,23 @@ const state = reactive({
   isLoading: true,
 })
 
-const noServers = computed(() => state.containers.length === 0)
-
 onMounted(async () => {
   try {
     const res = await fetch('/pythonapi/containers?all=true')
     const data = await res.json()
     state.containers = data
+    if (data.detail === "Not Found") {
+      throw new Error ('Pythonapi, Containers not found.') /* TODO Show specific error message on the web if the error is due and api not found */
+    }
   } catch (error) {
     console.error('Error fetching containers: ', error)
   } finally {
     state.isLoading = false
   }
 })
+
+const noServers = computed(() => state.containers.length === 0)
+const notFound = computed(() => state.containers.detail === "Not Found")
 </script>
 
 <template>
@@ -32,8 +36,9 @@ onMounted(async () => {
       <PulseLoader />
     </div>
 
-    <ul>
-      <span v-if="noServers && state.isLoading">No Servers</span>
+    <span v-if="noServers && !state.isLoading">No Servers</span>
+    <span v-if="notFound && !state.isLoading">Python Api Error: Fetch URL Not Found</span>
+    <ul v-if="(!noServers && !notFound) && !state.isLoading">
       <ServersListingCard
         v-for="container in state.containers"
         :key="container.id"
