5c256d5c1eb5e080dbd169879ba97e33bce213da
HY_ElCositas <erikvvh06@gmail.com>
2025-09-22 09:46:05 +0200

    Future recomendation

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -7,6 +7,7 @@ import ServersListing from '../components/ServersListing.vue'
       class="p-6 col-span-2 flex flex-col w-sm bg-em-gray-darker border-r border-green-500"
       style="height: calc(100dvh - 81px)"
     >
+      <!-- Add a preference in the settings to get a version w no background and no border, just as the original -->
       <ServersListing />
     </aside>
     <section class="p-6 col-span-6 flex flex-col">
